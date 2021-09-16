eeef474b09ade5255cc411352f78d843b111278a
Michael Nikitenko <xm4dn355x@gmail.com>
2021-09-16 18:34:18 +0500

    demo of reading xls and matching winner number

--- services.py
+++ services.py
@@ -9,9 +9,16 @@
 ########################################################################
 
 
+from collections import namedtuple
 from datetime import datetime
 
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ContentType
+from openpyxl import load_workbook
+
+
+WINNERS_XLS = 'Призы.xlsx'
+
+Winner = namedtuple('Winner', ['number', 'prizes', 'address'])
 
 
 def get_day_period(request: datetime):
@@ -20,13 +27,23 @@ def get_day_period(request: datetime):
     return periods[request.hour // 6]  # Сутки делятся на 6 равных частей, естественно разбиваясь на периоды
 
 
-def match_winner_number(number: int) -> list or bool:
+def read_xls(document_path: str) -> set:
+    """Читает xlsx файл и возвращает его содержимое в виде кортежа из namedtuple Winner"""
+    wb = load_workbook(document_path, read_only=True)
+    ws = wb.active
+    res = tuple([Winner(row[0].value, row[1].value, row[2].value) for row in ws.rows if isinstance(row[0].value, int)])
+    return res
+
+
+def match_winner_number(number: int) -> str:
     """Получает номер билета и возвращает словарь с результатами"""
-    if number == 10:  # TODO: Заменить мокнутое сообщение на нормальное
-        return ['Кружка', 'Худи', 'Трусы']
-    elif number == 100:
-        return ['Штаны']
-    return False
+    print(type(number))
+    winners = read_xls(document_path=WINNERS_XLS)
+    for w in winners:
+        if w.number == number:
+            return f'Поздравляем! Вы выиграли: {w.prizes}! Вы можете получить свои подарки по адресу: {w.address}'
+    return 'К сожалению не видим вашего билета среди списка победителей.\nМожет опечатались? ' \
+           'В любой момент можете отправить номер билета боту и узнать результат'# FIXME: Дополнить про обратную связь
 
 
 def find_closest_place(location: ContentType.LOCATION):
@@ -59,3 +76,8 @@ yamalsky = KeyboardButton('Ямальский район')
 greet_kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 # greet_kb.add(button_geo).add(button1).row(button2, button3, button4).insert(button5).add(button6)
 greet_kb.row(shd, gub, lbt).row(mur, nur, noy).row(krasn, priural).row(nadym).row(pur, taz).row(shur, yamalsky)
+
+if __name__ == '__main__':
+    data = read_xls('Призы.xlsx')
+    for d in data:
+        print(d)
