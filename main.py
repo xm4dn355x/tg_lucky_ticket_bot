c289d131ae79f678d05c2ee0f9de391ed1832124
Michael Nikitenko <xm4dn355x@gmail.com>
2021-09-17 17:59:43 +0500

    feedback experiments

--- main.py
+++ main.py
@@ -15,12 +15,14 @@ from datetime import datetime
 from sys import platform
 
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.types import Message, ContentType
 from loguru import logger
 
-from feedback import Feedback
-from services import get_day_period, match_winner_number, greet_kb, find_closest_place
+from feedback import Feedback, cancel_kb
+from services import get_day_period, match_winner_number, find_closest_place
 
 # Init loggers
 logger.add('logs/debug.log', format="{time} {level} {message}", level='DEBUG', rotation='500 MB')
@@ -35,7 +37,7 @@ else:                       # If linux, or Docker, or MacOS you can use environm
 
 # Init aiogram bot
 bot = Bot(TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 @logger.catch
@@ -45,6 +47,67 @@ async def welcome(message: Message):
     await message.answer(f'{get_day_period(datetime.now())} Я бот конкурса *Уютный Ямал!*\n\n'
                         f'Отправь мне номер своего билета и я тебе скажу какие призы ты можешь получить',
                         parse_mode='Markdown')
+
+
+@logger.catch
+@dp.message_handler(commands=['feedback'])
+async def feedback_start(message: Message):
+    """Запуск формы обратной связи"""
+    await message.answer('Вы попали в форму обратной связи. ', reply_markup=cancel_kb)
+    await Feedback.waiting_for_phone.set()
+
+
+@dp.message_handler(state='*', commands=['cancel', 'Отмена'])
+async def cancel_handler(message: types.Message, state: FSMContext):    # FIXME: Разобраться почему это говно не работает с кнопки
+    """Позволяет пользователю отменить работу с формой"""
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+    logger.info('Cancelling state %r', current_state)
+    await state.finish()
+    await message.reply('Операция отменена', reply_markup=types.ReplyKeyboardRemove())
+
+
+@logger.catch
+@dp.message_handler(state=Feedback.waiting_for_phone)
+async def feedback_phone_set(message: Message, state: FSMContext):
+    """Пользователь ввёл номер телефона"""
+    try:
+        phone = int(message.text.strip().replace('+', ''))
+    except ValueError:
+        await message.answer('Ты долбоёб, чи шо? Нормально номер введи.')
+        return
+    await state.update_data(phone=phone)
+    await Feedback.next()
+    await message.answer('Теперь нужен номер билета')
+
+
+@logger.catch
+@dp.message_handler(state=Feedback.waiting_for_ticket)
+async def feedback_ticket_set(message: Message, state: FSMContext):
+    """Пользователь ввёл номер билета"""
+    try:
+        ticket = message.text.strip()
+    except ValueError:
+        await message.answer('Ты долбоёб, чи шо? Нормально номер билета введи.')
+        return
+    await state.update_data(ticket=ticket)
+    await Feedback.next()
+    await message.answer('Теперь текст обращения')
+
+
+@logger.catch
+@dp.message_handler(state=Feedback.waiting_for_text)
+async def feedback_text_set(message: Message, state: FSMContext):
+    """Пользователь ввёл текст вопроса"""
+    text = message.text.strip()
+    await state.update_data(text=text)
+    user_data = await state.get_data()
+    await message.answer(f"Ваше обращение:\n\nТелефон: {user_data['phone']}\nНомер билета: {user_data['ticket']}\n"
+                         f"Текст обращения: {user_data['text']}\n\n"
+                         f"Спасибо за обращение! "
+                         f"В скором времени с Вами свяжутся по указанному в заявке номеру телефона")
+    await state.finish()
 
 
 @logger.catch
@@ -68,28 +131,8 @@ async def wrong_number(message: Message):
     await message.answer('введите пожалуйста номер билета правильно без пробелов, номер может содержать только цифры')
 
 
-# @logger.catch
-# async def feedback_start(message: Message):
-#     """Запуск формы обратной связи"""
-#     await message.answer('Начало формы обратной связи. ебани телефон')
-#     await Feedback.waiting_for_phone.set()
-#
-#
-# @logger.catch
-# async def feedback_phone_set(message: Message, state: FSMContext):
-#     """Пользователь ввёл номер телефона"""
-#     try:
-#         phone = message.text.strip().replace('+', '')
-#     except ValueError:
-#         await message.answer('Ты долбоёб, чи шо? Нормально номер введи.')
-#         return
-#     await state.update_data(phone=phone)
-#     await message.answer('Теперь нужен номер билета')
-# TODO: Переписать фидбэк на пересылание сообщения в чат
-
-
 if __name__ == '__main__':
     logger.info('Bot started')
     executor.start_polling(dp, skip_updates=True)
 
-# TODO: Обработчик для обратной связи на FSM Контактные данные, номер билета, текст обращения
+# TODO: Обработчик для обратной связи, добавить отправку на email
