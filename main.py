5f69bccf749a1c10052c9b6f2a27bff0546aa5c7
Michael Nikitenko <xm4dn355x@gmail.com>
2021-09-21 11:27:35 +0500

    delete less code and handled BotBlocked exception

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from sys import platform
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.types import Message
+from aiogram.utils.exceptions import BotBlocked
 from loguru import logger
 
 from services import get_day_period, match_winner_number
@@ -46,105 +47,28 @@ async def welcome(message: Message):
                          parse_mode='Markdown')
 
 
-# @logger.catch
-# @dp.message_handler(commands=['place'])
-# async def pickup_places(message: Message):
-#     """Клавиатура помогающая найти пункты выдачи"""
-#     places = services.read_xls_places(document_path='Пункты выдачи.xlsx')
-#     await message.reply('Пункты выдачи', reply_markup=services.generate_pickup_points_kb(places))
-#
-#
-# @dp.callback_query_handler(lambda c: c.data and c.data.startswith('btn'))
-# async def places_by_query(callback_query: types.CallbackQuery):
-#     """Обработчик кнопок в меню пунктов выдачи"""
-#     # places = services.read_xls_places(document_path='Пункты выдачи.xlsx')
-#     # await message.reply('Пункты выдачи', reply_markup=services.generate_pickup_points_kb(places, mo=))
-#     print('place by query')
-#     await bot.answer_callback_query(callback_query.id, text=f'Ты пидор {callback_query.data[-1]}')
-#
-#
-# @logger.catch
-# @dp.message_handler(commands=['feedback'])
-# async def feedback_start(message: Message):
-#     """Запуск формы обратной связи"""
-#     await message.answer('Вы попали в форму обратной связи. ', reply_markup=cancel_kb)
-#     await Feedback.waiting_for_phone.set()
-#
-#
-# @dp.message_handler(state='*', commands=['cancel', 'Отмена'])
-# async def cancel_handler(message: types.Message, state: FSMContext):
-#     """Позволяет пользователю отменить работу с формой"""
-#     current_state = await state.get_state()
-#     if current_state is None:
-#         return
-#     logger.info('Cancelling state %r', current_state)
-#     await state.finish()
-#     await message.reply('Операция отменена', reply_markup=types.ReplyKeyboardRemove())
-#
-#
-# @logger.catch
-# @dp.message_handler(state=Feedback.waiting_for_phone)
-# async def feedback_phone_set(message: Message, state: FSMContext):
-#     """Пользователь ввёл номер телефона"""
-#     try:
-#         phone = int(message.text.strip().replace('+', ''))
-#     except ValueError:
-#         await message.answer('Ты долбоёб, чи шо? Нормально номер введи.')
-#         return
-#     await state.update_data(phone=phone)
-#     await Feedback.next()
-#     await message.answer('Теперь нужен номер билета')
-#
-#
-# @logger.catch
-# @dp.message_handler(state=Feedback.waiting_for_ticket)
-# async def feedback_ticket_set(message: Message, state: FSMContext):
-#     """Пользователь ввёл номер билета"""
-#     try:
-#         ticket = message.text.strip()
-#     except ValueError:
-#         await message.answer('Ты долбоёб, чи шо? Нормально номер билета введи.')
-#         return
-#     await state.update_data(ticket=ticket)
-#     await Feedback.next()
-#     await message.answer('Теперь текст обращения')
-#
-#
-# @logger.catch
-# @dp.message_handler(state=Feedback.waiting_for_text)
-# async def feedback_text_set(message: Message, state: FSMContext):
-#     """Пользователь ввёл текст вопроса"""
-#     text = message.text.strip()
-#     await state.update_data(text=text)
-#     user_data = await state.get_data()
-#     await message.answer(f"Ваше обращение:\n\nТелефон: {user_data['phone']}\nНомер билета: {user_data['ticket']}\n"
-#                          f"Текст обращения: {user_data['text']}\n\n"
-#                          f"Спасибо за обращение! "
-#                          f"В скором времени с Вами свяжутся по указанному в заявке номеру телефона")
-#     await state.finish()
-
-
 @logger.catch
 @dp.message_handler(regexp=r'^\d+$')
 async def check_winner_or_not(message: Message):
     prizes_message = match_winner_number(message.text)
-    await message.answer(prizes_message, parse_mode='Markdown')
-
-
-# @logger.catch
-# @dp.message_handler(content_types=ContentType.LOCATION)
-# async def geo_handler(message: Message):
-#     result = find_closest_place(message.location)
-#     await message.answer(result)
+    try:
+        await message.answer(prizes_message, parse_mode='Markdown')
+    except BotBlocked as err:
+        print(f'Bot Blocked: {err.message} {message.from_user}')
+        logger.error(f'Bot Blocked: {err.message} {message.from_user}')
 
 
 @logger.catch
 @dp.message_handler(regexp=r'\D')
 async def wrong_number(message: Message):
     """Если пользователь написал номер билета с ошибками"""
-    await message.answer(
-        'Введите пожалуйста номер бланка правильно без пробелов, номер может содержать только цифры\n\n'
-        'Если у Вас возникли какие-то дополнительные вопросы - обращайтесь к нам по электронной почте: uyamal@bk.ru')
+    try:
+        await message.answer(
+            'Введите пожалуйста номер бланка правильно без пробелов, номер может содержать только цифры\n\n'
+            'Если у Вас возникли какие-то дополнительные вопросы - обращайтесь к нам по электронной почте: uyamal@bk.ru')
+    except BotBlocked as err:
+        print(f'Bot Blocked: {err.message} {message.from_user}')
+        logger.error(f'Bot Blocked: {err.message} {message.from_user}')
 
 
 if __name__ == '__main__':
